e9a121e16e201648fca97b8f645aead1a57f35d6
breemark <britomaximiliano83@gmail.com>
2023-08-03 16:21:16 +0700

    Create message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -21,6 +22,13 @@ class MessageController extends Controller
 
     public function store(Request $request)
     {
-        return $request->all();
+
+        Message::create([
+            'sender_id' => auth()->id(),
+            'recipient_id' => $request->recipient_id,
+            'body' => $request->body
+        ]);
+
+        return back()->with('flash', 'Your message has been sent');
     }
 }
